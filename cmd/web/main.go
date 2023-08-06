14cbeeea016fdfa3210e8f5bda2177a0fd95ae65
Patryk Ptak <ptaszqq@gmail.com>
2023-08-06 10:36:33 +0100

    structure changes

---
+++ cmd/web/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	fmt.Println("Starting server on port :8080!")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
