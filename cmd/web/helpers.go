3b759826335c3bf797029dd1a3d04e91a6fd96b2
Patryk Ptak <ptaszqq@gmail.com>
2023-08-06 16:56:10 +0100

    git attributes

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "runtime/debug"
+	"fmt"
+	"net/http"
+	"runtime/debug"
 )
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
-    trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-    app.errorLog.Output(2, trace)
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.errorLog.Output(2, trace)
 
-    http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
 func (app *application) clientError(w http.ResponseWriter, status int) {
-    http.Error(w, http.StatusText(status), status)
+	http.Error(w, http.StatusText(status), status)
 }
 
 func (app *application) notFound(w http.ResponseWriter) {
-    app.clientError(w, http.StatusNotFound)
+	app.clientError(w, http.StatusNotFound)
 }
