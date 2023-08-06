f6ec84817918bb2bb2c46bc019f2d47b4a1b2190
Patryk Ptak <ptaszqq@gmail.com>
2023-08-06 15:49:41 +0100

    logging depenendy injection

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,54 +3,53 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	app.infoLog.Printf("Requesting URL Path: %s\n", r.URL.Path)
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
-  // Templates
-  tmplFiles := []string{
-    "./ui/html/base.tmpl.html",
-    "./ui/html/partials/nav.tmpl.html",
-    "./ui/html/home.tmpl.html",
-  }
+	// Templates
+	tmplFiles := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
+		"./ui/html/home.tmpl.html",
+	}
 
 	tmpl, err := template.ParseFiles(tmplFiles...)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, err)
 		return
 	}
 
 	err = tmpl.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, err)
 		return
 	}
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	app.infoLog.Printf("Requesting URL Path: %s\n", r.URL.Path)
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
 	fmt.Fprintf(w, "Dispaly a specific snippet with ID %d\n", id)
-	// w.Write([]byte())
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	app.infoLog.Printf("Requesting URL Path: %s\n", r.URL.Path)
 	if r.Method != "POST" {
 		w.Header().Set("Allow", "POST")
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("Create a new snippet..."))
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,22 +2,51 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
+	// FLAG PARSING
 	port := flag.String("port", ":8080", "HTTP Port to run server")
 	flag.Parse()
 
+	// LOGGERS
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		infoLog:  infoLog,
+		errorLog: errorLog,
+	}
+
+	// Multiplexer
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	// HANDLERS
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+
+	// STATIC HANDLER
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	fmt.Printf("Starting server on port %s\n", *port)
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	// SERVER
+
+	srv := &http.Server{
+		Addr:     *port,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on port %s\n", *port)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
