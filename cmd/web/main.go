d47adafed920a874da9286b56ab38a1a17e11f38
Patryk Ptak <ptaszqq@gmail.com>
2023-08-06 13:48:53 +0100

    get port from flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	port := flag.String("port", ":8080", "HTTP Port to run server")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-  fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	fmt.Println("Starting server on port :8080!")
+	fmt.Printf("Starting server on port %s\n", *port)
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
