edd3a7db49f08697d5230cb689f1713d3d2c51ee
Patryk Ptak <ptaszqq@gmail.com>
2023-08-07 22:26:09 +0100

    custom function

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,14 +3,23 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"github.com/patrykptak/letsgo-server/internal/models"
 )
 
 type templateData struct {
-  CurrentYear int
-	Snippet  *models.Snippet
-	Snippets []*models.Snippet
+	CurrentYear int
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -24,15 +33,15 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.tmpl.html")
-    if err != nil {
-      return nil, err
-    }
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl.html")
+		if err != nil {
+			return nil, err
+		}
 
-    ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
-    if err != nil {
-      return nil, err
-    }
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
+		if err != nil {
+			return nil, err
+		}
 
 		ts, err = ts.ParseFiles(page)
 		if err != nil {
