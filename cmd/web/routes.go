a6b1c402042d07fe19cf3ed14acd686acd0c8374
Patryk Ptak <ptaszqq@gmail.com>
2023-08-08 18:54:39 +0100

    adds new router

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,19 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler {
-	// Multiplexer
-	mux := http.NewServeMux()
+	router := httprouter.New()
+  mux := http.NewServeMux()
 
 	// STATIC FILES HANDLER
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	// HANDLERS
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+  router.HandlerFunc(http.MethodGet, "/", app.home)
+  router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+  router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+  router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	return standard.Then(router)
 }
