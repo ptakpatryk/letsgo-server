9ea967cbf1453b673ac86e2bb169361e72e7fc75
Patryk Ptak <ptaszqq@gmail.com>
2023-08-08 19:05:46 +0100

    custom 404 message

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,15 +10,20 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	// Custom 404 response
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
+
 	// STATIC FILES HANDLER
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
 	// HANDLERS
-  router.HandlerFunc(http.MethodGet, "/", app.home)
-  router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
-  router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
-  router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
