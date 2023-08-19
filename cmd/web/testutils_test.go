b8b51342f7b874e4567120487de68d1b9e26e7d5
Patryk Ptak <ptaszqq@gmail.com>
2023-08-19 11:27:11 +0100

    sending form e2e test

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"bytes"
+	"html"
 	"io"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
+	"regexp"
+
 	// "os"
 	"testing"
 	"time"
@@ -16,28 +20,39 @@ import (
 	"github.com/patrykptak/letsgo-server/internal/models/mocks"
 )
 
+var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' value='(.+)'>`)
+
+func extractCSRFToken(t *testing.T, body string) string {
+	matches := csrfTokenRX.FindStringSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
+	}
+
+	return html.UnescapeString(string(matches[1]))
+}
+
 func newTestApplication(t *testing.T) *application {
-  templateCache, err := newTemplateCache()
-  if err != nil {
-    t.Fatal(err)
-  }
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		t.Fatal(err)
+	}
 
-  formDecoder := form.NewDecoder()
+	formDecoder := form.NewDecoder()
 
-  sessionManager := scs.New()
-  sessionManager.Lifetime = 12 * time.Hour
-  sessionManager.Cookie.Secure = true
+	sessionManager := scs.New()
+	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 
 	return &application{
 		// infoLog: log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
 		// errorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
-		infoLog:  log.New(io.Discard, "", 0),
-		errorLog: log.New(io.Discard, "", 0),
-    snippets: &mocks.SnippetModel{},
-    users: &mocks.UserModel{},
-    templateCache: templateCache,
-    formDecoder: formDecoder,
-    sessionManager: sessionManager,
+		infoLog:        log.New(io.Discard, "", 0),
+		errorLog:       log.New(io.Discard, "", 0),
+		snippets:       &mocks.SnippetModel{},
+		users:          &mocks.UserModel{},
+		templateCache:  templateCache,
+		formDecoder:    formDecoder,
+		sessionManager: sessionManager,
 	}
 }
 
@@ -48,17 +63,17 @@ type testServer struct {
 func newTestServer(t *testing.T, h http.Handler) *testServer {
 	ts := httptest.NewTLSServer(h)
 
-  jar, err := cookiejar.New(nil)
-  if err != nil {
-    t.Fatal()
-  }
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal()
+	}
 
-  ts.Client().Jar = jar
+	ts.Client().Jar = jar
 
-  ts.Client().CheckRedirect = func (req *http.Request, via []*http.Request) error  {
-    return http.ErrUseLastResponse
-  }
-  
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
 	return &testServer{ts}
 }
 
@@ -73,6 +88,23 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, strin
 	if err != nil {
 		t.Fatal(err)
 	}
+	body = bytes.TrimSpace(body)
+
+	return rs.StatusCode, rs.Header, string(body)
+}
+
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, string) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	body = bytes.TrimSpace(body)
 
 	return rs.StatusCode, rs.Header, string(body)
